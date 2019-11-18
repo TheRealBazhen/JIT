21f18fbee0fa2f3cfce59c2fd5316ffe30fe1d99
TheRealBazhen <andre.bazhenov30012001@gmail.com>
2019-11-18 15:17:42 +0300

    Add code coverage measurement

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,13 @@
 rm -rf ./build
 mkdir build
 cd build
-cmake .. -DCMAKE_C_COMPILER=arm-linux-gnueabi-gcc\
- -DCMAKE_CXX_COMPILER=arm-linux-gnueabi-g++
+cmake .. -DCMAKE_CXX_COMPILER=arm-linux-gnueabi-g++
 make
 qemu-arm -L $LINARO_SYSROOT ./JITtest
+cd CMakeFiles/JITtest.dir/
+gcov *.o
+lcov --directory . -c -o main.info
+cd ../../
+mkdir output
+cd output
+genhtml ../CMakeFiles/JITtest.dir/main.info
